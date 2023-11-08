0959462934c7884b4f46204ebb7e4879918302c2
laizabsobral <laiza.b.sobral@accenture.com>
2023-11-08 18:12:55 -0300

    create menu navigation

--- src/components/Sidebar/logo.tsx
+++ src/components/Sidebar/logo.tsx
@@ -1,6 +1,6 @@
 export function Logo() {
   return (
-    <strong className="flex items-center gap-2 text-xl font-semibold text-zinc-900">
+    <strong className="flex  mx-1 items-center gap-2 text-xl font-semibold text-zinc-900">
       <svg
         width={38}
         height={38}
