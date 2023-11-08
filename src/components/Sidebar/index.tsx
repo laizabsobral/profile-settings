bd985607fc1b2ff63fec9f68aa36253115003f75
laizabsobral <laiza.b.sobral@accenture.com>
2023-11-08 17:32:37 -0300

    created the project structure

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,9 @@
+import { Logo } from "./logo";
+
+export function Sidebar() {
+    return(
+        <aside className='border-r border-zinc-200 px-5 py-8'> 
+            <Logo/>
+        </aside>
+    )
+}
